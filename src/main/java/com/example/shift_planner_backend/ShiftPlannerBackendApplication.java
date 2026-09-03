f1c0dda81698acc88ee29c9952cdcb962d4e1656
Lakshmi Prasanna Cherukula <lakshmipcherukula@gmail.com>
2026-09-03 17:31:33 -0500

    Initial commit - set up spring boot and database configuration.

---
+++ src/main/java/com/example/shift_planner_backend/ShiftPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shift_planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shift_planner_backend/ShiftPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shift_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
