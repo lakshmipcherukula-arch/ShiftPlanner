3b3a7a975bd5fa61c7de5fbd69626592cba80d0b
Lakshmi Prasanna Cherukula <lakshmipcherukula@gmail.com>
2026-09-13 18:38:33 -0500

    Fixed Date formatting

--- src/main/java/com/example/shift_planner_backend/repositories/ShiftRepository.java
+++ src/main/java/com/example/shift_planner_backend/repositories/ShiftRepository.java
@@ -4,10 +4,11 @@ import com.example.shift_planner_backend.models.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
-    List<Shift> findByDateGreaterThanEqualAndIsAvailableTrue(Date date);
+    List<Shift> findByDateGreaterThanEqualAndIsAvailableTrue(LocalDate date);
 }
